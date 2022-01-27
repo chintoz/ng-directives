39f7795c74c549d9aa4ccda84690868806e486cc
Jacinto Jesús Mena Lomeña <jacintomena@gmail.com>
2022-01-28 00:49:40 +0100

    Changes to create a directive using a good practice to modify elements changing them with renderer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directive";
+import {BetterHighlightDirective} from "./better-highlight/better-highlight.directive";
 
 @NgModule({
   declarations: [
-    AppComponent, BasicHighlightDirective
+    AppComponent, BasicHighlightDirective, BetterHighlightDirective
   ],
   imports: [
     BrowserModule, FormsModule
