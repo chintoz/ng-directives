699b43c0b81ca79945bfc5014d3c76a2e05d217d
Jacinto Jesús Mena Lomeña <jacintomena@gmail.com>
2022-01-28 00:39:42 +0100

    Change to add our first custom directive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directive";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, BasicHighlightDirective
   ],
   imports: [
     BrowserModule, FormsModule
