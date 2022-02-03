dc00ef7acfdd8f30cfec2269ad7f9ad1a1479f0f
Jacinto Jesús Mena Lomeña <jacintomena@gmail.com>
2022-02-04 00:22:12 +0100

    Changes to add binding properties to a directive to be able to customize it.

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,28 +1,32 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHightlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent'
+  @Input() defaultColor: string = 'transparent';
+  // You are able to "reuse" directive name to provide value. In that case, directive name
+  // has to be enclose by [].
+  @Input('appBetterHightlight') highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string = ''
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
+    this.backgroundColor = this.defaultColor
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
     //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue')
-    this.backgroundColor = 'blue'
+    this.backgroundColor = this.highlightColor
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
-    this.backgroundColor = 'transparent'
+    this.backgroundColor = this.defaultColor
   }
-
 
 
 }
